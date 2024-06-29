285c06f0d7cad19def3cb71578349870440b300b
Remmy4873 <Remmykiplangat4873@gmail.com>
2024-06-29 15:32:49 +0300

    add arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+console.log(multiply);
+
+const random = Math.floor(Math.random() * 100) + 1;
+console.log(random);
+
+const num3 = 12;
+const num4 = 8;
+const mod = num3 % num4;
+console.log(mod);
+
+const max = Math.max(15, 16, 17, 18, 19, 20);
+console.log(max);
